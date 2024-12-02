94fcbbc0a4a6e9e194788ebf747a6713e394fc56
Дмитрий Золотухин <zolotuhindima2006@gmail.com>
2024-12-02 22:57:52 +0300

    Добавьте файлы проекта.

---
+++ SP_DZ3/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace SP_DZ3
+{
+    internal class Program
+    {
+        static int count = 0;
+        static void IncrementOperation() 
+        {
+            for (int i = 0; i < 100_000; i++)
+            {
+                count++;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            const int threadCount = 4;
+            Thread[] threads = new Thread[threadCount];
+            for (int i = 0; i < threadCount; i++)
+            {
+                threads[i] = new Thread(IncrementOperation);
+                threads[i].Start();
+            }
+            for (int i = 0; i < threadCount; i++)
+            {
+                threads[i].Join();
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
